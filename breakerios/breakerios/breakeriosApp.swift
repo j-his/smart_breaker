cffe4ab3c0384413e7d11307fcb998f7340c2637
Tong tong <t0ngx2@icloud.com>
2026-02-28 12:53:15 -0800

    added ios app fiiles

---
+++ breakerios/breakerios/breakeriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakeriosApp.swift
+//  breakerios
+//
+//  Created by Tong tong Wang on 28/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct breakeriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ breakerios/breakeriosTests/breakeriosTests.swift
@@ -0,0 +1,17 @@
+//
+//  breakeriosTests.swift
+//  breakeriosTests
+//
+//  Created by Tong tong Wang on 28/2/2026.
+//
+
+import Testing
+@testable import breakerios
+
+struct breakeriosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
