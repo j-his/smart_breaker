60fab3ccafa83bcccf6154b81bcf8f8e93303357
Tong tong <t0ngx2@icloud.com>
2026-02-28 16:05:02 -0800

    partial refresh cubes

--- ESP32/ESP32/src/main.cpp
+++ ESP32/ESP32/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>         // Include the core Arduino library to provide basic Arduino functionality
 #include "EPD.h"             // Include the EPD library for controlling the electronic ink screen (E-Paper Display)
 #include "pic_home.h"        // Include the header file containing image data
-#include "img/device_interface2.h" // Include the header file containing additional image data
+// #include "img/device_interface2.h" // Include the header file containing additional image data
 
 uint8_t ImageBW[27200];      // Declare an array of 27200 bytes to store black and white image data
 
@@ -9,12 +9,16 @@ uint8_t ImageTest[27200];
 void clear_all();
 
 uint8_t square[4900];
+uint8_t square2[4900];
 
 void display_image(uint8_t *image, int x, int y, int w, int h);
+void draw_image_to_buffer(uint8_t *image, int x, int y, int w, int h);
+void update_screen();
 
 void setup() {
   for(int i = 0; i < 4900; i++) {
-    square[i] = 0xFF;
+    square[i] = 0x00; // 0x00 is BLACK
+    square2[i] = 0xFF;
   }
   // Initialization settings, executed once when the program starts
   pinMode(7, OUTPUT);        // Set pin 7 to output mode
@@ -29,13 +33,37 @@ void setup() {
   EPD_Display_Clear();       // Clear the screen content
   EPD_Update();              // Update the screen display
 
-  display_image(gImage_device_interface2, 0, 0, 792, 272);
+  // display_image(gImage_device_interface2, 0, 0, 792, 272);
   delay(1000);
-  //display_image(square, 10, 10, 70, 70);
+  
+  // Center coordinates for a 70x70 square on a 792x272 screen
+  // x = (792 - 70) / 2 = 361
+  // y = (272 - 70) / 2 = 101
+  draw_image_to_buffer(square, 361, 101, 70, 70);
+  update_screen();
+  delay(1000);
+  
+  // Add more black squares around the center
+  draw_image_to_buffer(square, 261, 101, 70, 70); // Left square
+  update_screen();
+  delay(1000);
+
+  draw_image_to_buffer(square, 461, 101, 70, 70); // Right square
+  update_screen();
+  delay(1000);
+
+  draw_image_to_buffer(square, 361, 21,  70, 70); // Top square
+  update_screen();
+  delay(1000);
+
+  draw_image_to_buffer(square, 361, 181, 70, 70); // Bottom square
+  update_screen();
+  
   delay(5000);               // Wait for 5000 milliseconds (5 seconds)
 
   clear_all();               // Call the clear_all function to clear the screen content
 }
+
 
 void loop() {
   // Main loop function, currently does not perform any actions
@@ -68,9 +96,26 @@ void display_image(uint8_t *image, int x, int y, int w, int h) {
       
       EPD_Display(ImageBW);      // Display the image stored in the ImageBW array
       
-      EPD_FastUpdate();          // Perform a fast update to refresh the screen
+      //EPD_FastUpdate();          // Perform a fast update to refresh the screen
       EPD_DeepSleep();           // Set the screen to deep sleep mode to save power
     }
     
     free(temp); // Free the dynamically allocated memory
+}
+
+void draw_image_to_buffer(uint8_t *image, int x, int y, int w, int h) {
+    if(x + w <= 792 && y + h <= 272) {
+      for(int j = 0; j < h; j++) {
+        for(int i = 0; i < w; i++) {
+          Paint_SetPixel(x + i, y + j, image[j * w + i]);
+        }
+      }
+    }
+}
+
+void update_screen() {
+    EPD_HW_RESET();
+    EPD_Display(ImageBW);      // Display the image stored in the ImageBW array
+    EPD_PartUpdate();          // Update part of the screen to show the new content
+    EPD_DeepSleep();
 }
